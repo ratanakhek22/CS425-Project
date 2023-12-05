0ef260e98bbb5a32d5b710bd90b8ae15b599a72f
j-judeh <112597055+j-judeh@users.noreply.github.com>
2023-12-04 21:33:14 -0600

    yo

--- real_estate/models.py
+++ real_estate/models.py
@@ -1,45 +1,50 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class UserProfile(AbstractUser):
+    # Inherits fields from AbstractUser (username, password, etc.)
+    pass
 
-class UserProfile(AbstractUser, models.Model):
-    password = models.CharField(max_length=20)
-    username = models.CharField(max_length=30, unique=True)
-    
 class Company(models.Model):
     name = models.CharField(max_length=30)
     companyID = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="isCompany")
-    
-    def _str_(self):
+
+    def __str__(self):
         return self.name
-    
+
 class Agent(models.Model):
     name = models.CharField(max_length=30)
     phone = models.CharField(max_length=10)
     agentID = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="isAgent")
     companyID = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="allAgents")
-    
+
+    def __str__(self):
+        return self.name
+
 class Customer(models.Model):
     name = models.CharField(max_length=30)
     phone = models.CharField(max_length=10)
     customerID = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="isCustomer")
+
+    def __str__(self):
+        return self.name
 
 class Property(models.Model):
     zip = models.IntegerField()
     state = models.CharField(max_length=2)
     address = models.CharField(max_length=30)
     price = models.IntegerField()
-    desciption = models.TextField()
+    description = models.TextField()
+    ownerName = models.CharField(max_length=30)
     agentID = models.ForeignKey(Agent, on_delete=models.CASCADE, related_name="allAssignedProperties")
-    
+
 class Review(models.Model):
     author = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="myReviews")
     propertyID = models.ForeignKey(Property, on_delete=models.CASCADE, related_name="allReviews")
-    date = models.DateField() # datetime.date instance (python)
+    date = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
-    
+
 class Booking(models.Model):
     customerID = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="myAppointments")
     propertyID = models.ForeignKey(Property, on_delete=models.CASCADE, related_name="allBookings")
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
